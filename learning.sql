7fb3186ea97436da3842d09a8d9a542ca6ae6ec8
alexey <alexeygusak@gmail.com>
2018-10-01 11:54:55 +0300

    added 3th lesson

--- learning.sql
+++ learning.sql
@@ -72,5 +72,70 @@ SELECT * FROM cities LEFT JOIN regions ON (regions.id = cities.regions_id);
 SELECT cities.name, regions.name FROM cities LEFT JOIN regions ON (regions.id = cities.regions_id);
 SELECT cities.id as id , cities.name as city, regions.name as region FROM cities LEFT JOIN regions ON (regions.id = cities.regions_id) ORDER BY cities.id desc;
 
+//3
+ALTER TABLE cities ADD COLUMN strength INTEGER NOT NULL DEFAULT 0;
+SELECT * FROM cities // всем записям добавилось значение 0
+
+ALTER TABLE cities DROP COLUMN strength;
+
+ALTER TABLE cities CHANGE strength count INTEGER NOT NULL DEFAULT 0;//mysql
+ALTER TABLE cities RENAME strength to count;//postgresql
+
+ALTER TABLE cities MODIFY count VARCHAR(255) NOT NULL DEFAULT 'nope';//mysql
+ALTER TABLE cities ALTER COLUMN vars SET DATA TYPE VARCHAR(255);//posgresql
+ALTER TABLE cities ALTER COLUMN vars SET DEFAULT 'nope';//posgresql
+ALTER TABLE cities ALTER COLUMN vars SET NOT NULL;//posgresql
+
+SELECT * FROM cities WHERE name like 'Erazor';
+SELECT * FROM cities WHERE name like '%s';
+SELECT * FROM cities WHERE name like '%a%';
+SELECT * FROM cities WHERE regions_id = 2 AND name like '%a%';
+SELECT * FROM cities WHERE regions_id = 1 OR name like '%a%';
+
+SELECT DISTINCT regions_id FROM cities;
+SELECT * FROM cities GROUP BY regions_id HAVING strength > 40000;
+
+//func
+SELECT MIN(id) FROM cities;
+SELECT MAX(id) FROM cities;
+SELECT ROUND(1.2345);
+SELECT RANDOM();
+
+//func string
+SELECT LENGTH('132 21AR');//вывести кол-во символов
+SELECT TRIM('132 21AR');//вывести только литералы
+SELECT LENGTH(TRIM('132 21AR'));//вывести колво литералов
+SELECT UPPER('132 21ARad');
+SELECT LOWER('132 21ARad');
+SELECT SUBSTR('132 21ARad', 2);//вернуть строку со сдвигом на 1 символ
+SELECT REPLACE('a1a1a1a1', 1, 2);//заменить 1 на 2
+SELECT REVERSE('12345');
+SELECT MD5('12345');//вернуть хэш строки
+SELECT SHA2('12345', 224);//вернуть хэш строки, разрядностью 224
+
+//func time
+SELECT NOW();
+SELECT UNIX_TIMESTAMP();
+
+CREATE TABLE table1 (id INTEGER, name VARCHAR(255));
+INSERT INTO table1 VALUES (1, '123');
+INSERT INTO table1 VALUES (2, '123');
+INSERT INTO table1 VALUES (3, '123');
+INSERT INTO table1 VALUES (4, '123');
+INSERT INTO table1 VALUES (5, '123');
+
+SELECT * FROM table1;
+
+CREATE TABLE table2 (field_1 INTEGER, field_2 VARCHAR(255));
+INSERT INTO table2 SELECT id, name FROM table1;
+SELECT * FROM table2;
+DELETE FROM table2;
+INSERT INTO table2 SELECT id, name FROM table1 ORDER BY id desc;
+
+
+
+
+
+
 
 
